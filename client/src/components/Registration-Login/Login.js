ab7ed9d03f336c15e51338ca76618741ebd64124
Vernon Lewis <leesyncere@gmail.com>
2020-11-12 14:26:42 -0800

    registration and login

---
+++ client/src/components/Registration-Login/Login.js
@@ -0,0 +1,15 @@
+import Recact from "react";
+
+import classes from './StyledLogin.module.css'
+
+const Login = (props) => {
+   return(
+      <div className={classes.Login}>
+         <div className={classes.loginBox}>
+            
+         </div>
+      </div>
+   )
+}
+
+export default Login

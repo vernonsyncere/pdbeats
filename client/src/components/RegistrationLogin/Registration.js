acce310c130275789383ca8fff1105f17bdf1aff
Vernon Lewis <leesyncere@gmail.com>
2020-11-21 07:48:55 -0800

    changes

--- client/src/components/RegistrationLogin/Registration.js
+++ client/src/components/RegistrationLogin/Registration.js
@@ -3,12 +3,30 @@ import React from 'react';
 import classes from './StyledRegistration.module.css';
 
 const Registration = (props) => {
+   
+   let style  = {
+      color: ""
+   }
+
+   
+
+   let logorReg = true
+
+   const logOrRegHandler = () => {
+      if ( style.color === "blue"){
+         style  = {
+            color: "grey"
+         }
+      }
+      logorReg = !logorReg
+      console.log("made it", logorReg, style.color)
+   }
    return (
       <div className={ classes.Registration }>
          <div className={classes.formBox}>
             <div className={classes.loginReg}>
                <div className={classes.titles}>
-                  <h2>Login</h2>
+                  <h2 style={style} onClick={logOrRegHandler}>Login</h2>
                </div>
                <div className={classes.second}>
                   <h2>Register</h2>
@@ -16,11 +34,11 @@ const Registration = (props) => {
             </div>
             <form className={classes.formGrid}>
                   <div className={classes.formSection}>
-                     <lable>Email address</lable>
+                     <label>Email address</label>
                      <input type="text"/>
                   </div>
                   <div className={classes.formSection}>
-                     <lable>Password</lable>
+                     <label>Password</label>
                      <input type="password"/>
                   </div>
 

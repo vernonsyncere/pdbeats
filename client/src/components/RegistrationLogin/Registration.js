06c19918abb2fab5bdb3af1c72bd747253bc8e5c
Vernon Lewis <leesyncere@gmail.com>
2020-11-21 08:10:58 -0800

    styling changes/options between login and reg on Registration

--- client/src/components/RegistrationLogin/Registration.js
+++ client/src/components/RegistrationLogin/Registration.js
@@ -23,7 +23,7 @@ const Registration = (props) => {
    }
    return (
       <div className={ classes.Registration }>
-         <div className={classes.formBox}>
+         {/* <div className={classes.formBox}>
             <div className={classes.loginReg}>
                <div className={classes.titles}>
                   <h2 style={style} onClick={logOrRegHandler}>Login</h2>
@@ -44,7 +44,34 @@ const Registration = (props) => {
 
                   <button type="submit">Login</button>
                </form>
-               <p className={classes.question}>Lost your password?</p>
+               <p className={classes.question}>Lost your password?</p>  */}
+               <div className={classes.formBox}>
+            <div className={classes.loginReg}>
+               <div className={classes.titles}>
+                  <h2 style={style} onClick={logOrRegHandler}>Login</h2>
+               </div>
+               <div className={classes.second}>
+                  <h2>Register</h2>
+               </div>
+            </div>
+            <form className={classes.formGrid}>
+                  <div className={classes.formSection}>
+                     <label>Email address</label>
+                     <input type="text"/>
+                  </div>
+                  <div className={classes.formSection}>
+                     <label>Password</label>
+                     <input type="password"/>
+                  </div>
+                  <p className={classes.disclose}>
+                  Your personal data will be used to support your experience throughout this website, 
+                  to manage access to your account, and for other purposes described in our <span> privacy 
+                  policy</span>.
+                  </p>
+
+                  <button type="submit">Register</button>
+               </form>
+               {/* <p className={classes.question}>Lost your password?</p> */}
 
          </div>
       </div>

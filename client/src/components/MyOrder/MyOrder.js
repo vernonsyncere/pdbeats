45d84cc9e4d756630287719b1ff9e478b8300a1c
Vernon Lewis <leesyncere@gmail.com>
2020-12-10 16:58:34 -0800

    deleted unused code

--- client/src/components/MyOrder/MyOrder.js
+++ client/src/components/MyOrder/MyOrder.js
@@ -16,14 +16,14 @@ const MyOrder = (props) => {
                <p><span>View all of your purchases</span></p>
             </div>
 
-            {/* one order  */}
+            {/* map through all orders one order  */}
             <div className={ classes.purchase }>
                 <div className={ classes.purchaseLine }>
                    <div className={classes.headlines}>Product</div> 
                    <div className={classes.headlines}>Download</div> 
                    <div className={classes.headlines}>Purchase Date</div> 
                    
-                   {/* Here would start the map to through orders */}
+                   {/* Here would start the map through specific order */}
                    <div className={classes.headlines}>Order Number</div> 
                    <div className={classes.product}> <img src={night}/></div>     
                    <div className={classes.everything}><a href="https://www.beatstars.com/ProduceDepartmentBeats/sound-kits/48260" target="_blank">The Night of</a></div>   
@@ -33,34 +33,6 @@ const MyOrder = (props) => {
                    <div className={classes.everything}><a href="https://www.beatstars.com/ProduceDepartmentBeats/sound-kits/48260" target="_blank">The Night of</a></div>   
                    <div className={classes.everything}><p>December 10th, 2020</p></div>   
                    <div className={classes.everything}><p>BFGkLPOBM</p></div>    
-                  {/* <div className={ classes.left }>
-                     <p>Purchase Date</p>
-                     <p className={ classes.date }>November 29, 2020</p>
-                  </div>
-                  <div className={ classes.right }>
-                     <p>Order number</p>
-                     <p className={ classes.number }>BGIJ90JY32</p>
-                  </div> */}
-               </div> 
-               
-               {/* <div className={ classes.purchaseLine }> */}
-                  {/* <div className={ classes.left }>
-                     <p>Product</p>
-                     <p className={ classes.date }>Title</p>
-                  </div>
-                  <div className={ classes.right }>
-                     <p>Quantity</p>
-                     <p className={ classes.number }>1</p>
-                  </div> */}
-               {/* </div> */}
-               {/* <div className={ classes.purchaseLine }>
-                  <div className={ classes.left }>
-                     <p>Order Total</p>
-                  </div>
-                  <div className={ classes.right }>
-                     <p>$100.00</p>
-                  </div>
-               </div> */}
             </div>
             <div className={ classes.purchase }>
                 <div className={ classes.purchaseLine }>
@@ -78,34 +50,6 @@ const MyOrder = (props) => {
                    <div className={classes.everything}><a href="https://www.beatstars.com/ProduceDepartmentBeats/sound-kits/48260" target="_blank">The Night of</a></div>   
                    <div className={classes.everything}><p>December 10th, 2020</p></div>   
                    <div className={classes.everything}><p>BFGkLPOBM</p></div>    
-                  {/* <div className={ classes.left }>
-                     <p>Purchase Date</p>
-                     <p className={ classes.date }>November 29, 2020</p>
-                  </div>
-                  <div className={ classes.right }>
-                     <p>Order number</p>
-                     <p className={ classes.number }>BGIJ90JY32</p>
-                  </div> */}
-               </div> 
-               
-               {/* <div className={ classes.purchaseLine }> */}
-                  {/* <div className={ classes.left }>
-                     <p>Product</p>
-                     <p className={ classes.date }>Title</p>
-                  </div>
-                  <div className={ classes.right }>
-                     <p>Quantity</p>
-                     <p className={ classes.number }>1</p>
-                  </div> */}
-               {/* </div> */}
-               {/* <div className={ classes.purchaseLine }>
-                  <div className={ classes.left }>
-                     <p>Order Total</p>
-                  </div>
-                  <div className={ classes.right }>
-                     <p>$100.00</p>
-                  </div>
-               </div> */}
             </div>
             
          </div>

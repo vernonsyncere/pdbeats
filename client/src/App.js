7ac70f9fbbfcea19fe7a44d50b4b5db987eab5a9
Vernon Lewis <leesyncere@gmail.com>
2020-12-12 21:06:19 -0800

    minor changes to fix some errors

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,8 @@ const App = (props) => {
         {/* <BeatStore/> */}
       {/* <RegistrationLogin/> */}
       {/* <MyOrder></MyOrder> */}
-      {/* <ClientProfile></ClientProfile> */}
-      <AdminDashBoard/>
+      <ClientProfile></ClientProfile>
+      {/* <AdminDashBoard/> */}
     </div>
   );
 }
